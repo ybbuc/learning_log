4f6543994fa75ad0401f0a799f405fbd8e5959c7
Jakob Wells <jakobwithak@gmail.com>
2022-06-08 20:16:18 +0800

    Update datavase settings to use PostgreSQL

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = "learning_log.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "cubby",
+        "USER": "cubby",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
