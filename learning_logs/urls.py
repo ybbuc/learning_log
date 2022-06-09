e5ed39e5ac36dcd724c90d97b071d08fade3716d
Jakob Wells <jakobwithak@gmail.com>
2022-06-09 19:46:35 +0800

    Add a home page

---
+++ learning_logs/urls.py
@@ -0,0 +1,10 @@
+"""Defines URL pattersnf ro learning_logs."""
+
+from django.urls import path
+from . import views
+
+app_name = "learning_logs"
+urlpatterns = [
+    # Home page
+    path("", views.index, name="index"),
+]
