1bf263c1bdb645752d1212dcc58b00c5ffaaa89c
Jakob Wells <jakobwithak@gmail.com>
2022-06-11 10:44:03 +0800

    Add a form for editing existing entries

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
 
-from .models import Topic
-from .forms import TopicForm
+from .forms import EntryForm, TopicForm
+from .models import Entry, Topic
 
 
 def index(request):
@@ -39,3 +39,43 @@ def new_topic(request):
     # Display a blank or invalid form.
     context = {"form": form}
     return render(request, "learning_logs/new_topic.html", context)
+
+
+def new_entry(request, topic_id):
+    """Add a new entry for a particular topic."""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != "POST":
+        # No data sunmitted create a blank form.
+        form = EntryForm()
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect("learning_logs:topic", topic_id=topic_id)
+
+    # Display a blank or invalid form.
+    context = {"topic": topic, "form": form}
+    return render(request, "learning_logs/new_entry.html", context)
+
+
+def edit_entry(request, entry_id):
+    """Edit an existing entry."""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != "POST":
+        # Initial request; pre-fill with the current entry.
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submittedl process data.
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("learning_logs:topic", topic_id=topic.id)
+
+    context = {"entry": entry, "topic": topic, "form": form}
+    return render(request, "learning_logs/edit_entry.html", context)
