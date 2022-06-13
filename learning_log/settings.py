745b72f01f9f8ab1d2cc549062c9960818842197
Jakob Wells <jakob@duck.com>
2022-06-13 15:59:23 +0800

    Restrict access to most pages to logged-in users

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -145,3 +145,5 @@ STATICFILES_FINDERS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_URL = "users:login"
